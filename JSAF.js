7116d3afd4552811145bf8e6d1dc2c3904d5be59
agronaut000 <56070208+agronaut000@users.noreply.github.com>
2020-10-30 01:28:20 +0300

    Update JSAF.js

--- JSAF.js
+++ JSAF.js
@@ -1356,18 +1356,18 @@ async function sendAnswerTemplate2(word, flag = 0) {
 	if(k.title == word)
 		txt = k.text
 }))
+	txt = txt.split("<br>↵").join('\n')
+	txt = txt.split("&nbsp;").join(' ')
+	txt = txt.split("<br />").join('\n')
+	txt = txt.split('<a').join('TMPaTMP').split('</a').join('TMPENDaTMEPEND')
+	txt = txt.replace(/<\/?[^>]+>/g,'')
+	txt = txt.split('TMPaTMP').join('<a').split('TMPENDaTMEPEND').join('</a')
+	
 	if(document.getElementById('msg1').innerHTML == "Доработать" && flag == 0) {
 		document.getElementById('inp').value = txt
 		template_flag = 1
 		template_flag2 = 1
 	} else {
-		txt = txt.split("<br>↵").join('\n')
-		txt = txt.split("&nbsp;").join(' ')
-		txt = txt.split("<br />").join('\n')
-		txt = txt.split('<a').join('TMPaTMP').split('</a').join('TMPENDaTMEPEND')
-		txt = txt.replace(/<\/?[^>]+>/g,'')
-		txt = txt.split('TMPaTMP').join('<a').split('TMPENDaTMEPEND').join('</a')
-		
 		txt = txt.split("\"").join("\\\"")
 		txt2 = txt.split('\n')
 		txt3 = ""
