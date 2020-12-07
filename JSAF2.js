6567afd5cbbdc2738ceb1a20f5adb9f6414830e0
agronaut000 <56070208+agronaut000@users.noreply.github.com>
2020-12-07 19:05:36 +0300

    Update JSAF2.js

--- JSAF2.js
+++ JSAF2.js
@@ -2267,12 +2267,15 @@ function prepTp() {
 	buttonGetStat.style.marginRight = "15px";
 	buttonGetStat.onclick = function() {
 		if(this.textContent == 'Скрыть стату') {
-			if(document.getElementById('tableStats') != undefined)
+			if(document.getElementById('tableStats') != undefined) {
 				document.getElementById('tableStats').remove()
+				document.getElementById('root').children[0].children[1].children[0].children[1].children[1].style.display = ""
+			}
 			this.textContent = 'Статистика'
 		} else {
 			getStats()
 			this.textContent = 'Скрыть стату'
+			document.getElementById('root').children[0].children[1].children[0].children[1].children[1].style.display = "none"
 		}
 	}
 	var btnAdd = document.getElementsByClassName('app-body-content-user_menu')[0].childNodes[0]
