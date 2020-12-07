82a81efecc0bc556fa319e873aeb0f9c466a945e
agronaut000 <56070208+agronaut000@users.noreply.github.com>
2020-12-07 19:22:13 +0300

    Update JSAF2.js

--- JSAF2.js
+++ JSAF2.js
@@ -2270,14 +2270,27 @@ function prepTp() {
 			if(document.getElementById('tableStats') != undefined) {
 				document.getElementById('tableStats').remove()
 			}
-			if(document.getElementById('root').children[0].children[1].children[0].children[1].children[1] != undefined)
-				document.getElementById('root').children[0].children[1].children[0].children[1].children[1].style.display = ""
 			this.textContent = 'Статистика'
+			
+			
+			if(window.location.href.indexOf('skyeng.autofaq.ai/tickets/assigned') != -1) {
+				document.getElementById('root').children[0].children[1].children[0].children[1].children[1].style.display = ""
+			}
+			if(window.location.href.indexOf('skyeng.autofaq.ai/tickets/archive') != -1) {
+				document.getElementById('root').children[0].children[1].children[0].children[1].children[0].style.display = ""
+			}
 		} else {
+			if(window.location.href.indexOf('skyeng.autofaq.ai/tickets/assigned') != -1) {
+				document.getElementById('root').children[0].children[1].children[0].children[1].children[1].style.display = "none"
+			} else if(window.location.href.indexOf('skyeng.autofaq.ai/tickets/archive') != -1) {
+				document.getElementById('root').children[0].children[1].children[0].children[1].children[0].style.display = "none"
+			} else {
+				this.textContent = 'Неверная страница'
+				setTimeout(function() { document.getElementById('buttonGetStat').textContent = Статистика }, 2000)
+				return
+			}
 			getStats()
 			this.textContent = 'Скрыть стату'
-			if(document.getElementById('root').children[0].children[1].children[0].children[1].children[1] != undefined)
-				document.getElementById('root').children[0].children[1].children[0].children[1].children[1].style.display = "none"
 		}
 	}
 	var btnAdd = document.getElementsByClassName('app-body-content-user_menu')[0].childNodes[0]
