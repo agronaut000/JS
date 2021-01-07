77627c3137440d9a42274ca4041d175e952a0478
agronaut000 <agronaut001@gmail.com>
2021-01-07 16:15:37 +0300

    Update JSAF3.js

--- JSAF3.js
+++ JSAF3.js
@@ -1578,7 +1578,7 @@ function startTimer() {
 					var stringInfo = ""
 					for(let i = 0; i < document.getElementsByClassName('expert-user_details-list')[1].childElementCount; i++) {
 						if(document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].firstChild.textContent == "id") {
-							userId = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].textContent
+							userId = document.getElementsByClassName('expert-user_details-list')[1].childNodes[i].childNodes[1].textContent.split(' ')[0]
 							break
 						}
 					}
@@ -2331,7 +2331,7 @@ async function getStats() {
 	table.style = 'table-layout: auto; width:750px;'
 	table.style.textAlign = 'center'
 	table.id = 'tableStats'
-	let columnNames = ["Оператор", "Закрыл запросов", "Среднее время ожидания", "Среднее время работы"]
+	let columnNames = ["Оператор", "Закрыл запросов", "Пощупал чатов", "Среднее время ожидания", "Среднее время работы"]
 	let trHead = document.createElement('tr')
 	for(let i = 0; i < columnNames.length; i++) {
 		var th = document.createElement('th')
@@ -2372,6 +2372,66 @@ async function getStats() {
 	array.sort(function (a, b) {
 		return b.conversationClosed - a.conversationClosed;
 	});
+	
+	var operatorId = []
+	var operatorNames = []
+	await fetch("https://skyeng.autofaq.ai/api/operators/statistic/currentState", {
+	  "credentials": "include"
+	}).then(result => b = result.json()).then(b => b.rows.forEach(k => {
+		if(k.operator.kbs.indexOf(120181) != -1 && k.operator.fullName.split('-')[0] == "ТП") {
+			operatorId.push(k.operator.id)
+			operatorNames.push(k.operator.fullName)
+		}
+	}))
+
+	var date = new Date()
+	day = month = ""
+	if(date.getMonth() < 9)
+	month = "0" + (date.getMonth() + 1)
+	else 
+	month = (date.getMonth() + 1)
+	if(date.getDate() < 10)
+	day = "0" + date.getDate()
+	else
+	day = date.getDate()
+
+	var secondDate = date.getFullYear() + "-" + month + "-" + day + "T20:59:59.059z"
+	date = date - 24 * 60 * 60 * 1000
+	var date2 = new Date()
+	date2.setTime(date)
+
+	if(date2.getMonth() < 9)
+	month2 = "0" + (date2.getMonth() + 1)
+	else 
+	month2 = (date2.getMonth() + 1)
+	if(date2.getDate() < 10)
+	day2 = "0" + date2.getDate()
+	else
+	day2 = date2.getDate()
+
+	var firstDate = date2.getFullYear() + "-" + month2 + "-" + day2 + "T21:00:00.000z"
+
+	var operatorChatCount = []
+	for(var l = 0; l < operatorId.length; l++) {
+	await fetch("https://skyeng.autofaq.ai/api/conversations/history", {
+				  "headers": {
+					"accept": "*/*",
+					"accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+					"cache-control": "max-age=0",
+					"content-type": "application/json",
+					"sec-fetch-dest": "empty",
+					"sec-fetch-mode": "cors",
+					"sec-fetch-site": "same-origin"
+				  },
+				  "referrer": "https://skyeng.autofaq.ai/logs",
+				  "referrerPolicy": "strict-origin-when-cross-origin",
+				  "body": "{\"serviceId\":\"361c681b-340a-4e47-9342-c7309e27e7b5\",\"mode\":\"Json\",\"participatingOperatorsIds\":[\""+operatorId[l]+"\"],\"tsFrom\":\"" + firstDate + "\",\"tsTo\":\"" + secondDate + "\",\"orderBy\":\"ts\",\"orderDirection\":\"Asc\",\"page\":1,\"limit\":1}",
+				  "method": "POST",
+				  "mode": "cors",
+				  "credentials": "include"
+				}).then(a => a.json()).then(b => operatorChatCount[l] = b.total)
+	}
+	
 	let tbody = document.createElement('tbody')
 	for(let i = 0; i < array.length; i++) {
 		var tr = document.createElement('tr')
@@ -2383,14 +2443,21 @@ async function getStats() {
 					td.style = 'text-align: left; padding-left: 50px'
 					break;
 				case 1:
-					td.textContent = array[i].conversationClosed;
+					for(let j = 0; j < operatorNames.length; j++)
+						if(array[i].operator == operatorNames.length) {
+							td.textContent = operatorChatCount[j]
+							break
+						}
 					break;
 				case 2:
+					td.textContent = array[i].conversationClosed;
+					break;
+				case 3:
 					var averageAnswerTime = Math.floor(array[i].averageAnswerTime / 1000)
 					averageAnswerTime = averageAnswerTime < 60 ? '00:' + averageAnswerTime : Math.floor(averageAnswerTime / 60) + ':' + ((averageAnswerTime % 60) < 10 ? '0' + (averageAnswerTime % 60) : (averageAnswerTime % 60))
 					td.textContent = averageAnswerTime;
 					break;
-				case 3:
+				case 4:
 					var averageHandlingTime = Math.floor(array[i].averageHandlingTime / 1000)
 					averageHandlingTime = averageHandlingTime < 60 ? averageHandlingTime : Math.floor(averageHandlingTime / 60) + ':' + ((averageHandlingTime % 60) < 10 ? '0' + (averageHandlingTime % 60) : (averageHandlingTime % 60))
 					td.textContent = averageHandlingTime;
@@ -2400,6 +2467,17 @@ async function getStats() {
 		}
 		tbody.append(tr)
 	}
+	
+	
+	for(let i = 0; i < tbody.childElementCount; i++) {
+		for(let j = 0; j < operatorNames.length; j++)
+			if(tbody.children[0].children[0] == operatorNames.length) {
+				let tr = document.createElement('tr')
+				tr.textContent = operatorChatCount[j]
+				tbody.children[0].insertBefore(tbody.children[0].children[2])
+			}
+	}
+	
 	table.append(trHead)
 	table.append(tbody)
 	document.getElementById('root').children[0].children[1].children[0].children[1].append(table)
